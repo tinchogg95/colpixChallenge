78debb4c26d7022c621599528fc398880a3b0928
tinchogg95 <tinchogimenez95@gmail.com>
2025-05-09 12:51:05 -0300

    exporting expiration time

--- src/main/java/com/colpix/challenge/security/JwtUtil.java
+++ src/main/java/com/colpix/challenge/security/JwtUtil.java
@@ -2,6 +2,7 @@ package com.colpix.challenge.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -11,13 +12,15 @@ import java.util.Date;
 public class JwtUtil {
 
     private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private final long EXPIRATION_TIME = 5 * 60 * 1000; // 5 minutos
+    
+    @Value("${jwt.expiration-time}")
+    private long expirationTime; 
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(key)
                 .compact();
     }
@@ -29,5 +32,10 @@ public class JwtUtil {
         } catch (JwtException e) {
             return null;
         }
+    }
+
+    // Getter para acceder al expirationTime si es necesario
+    public long getExpirationTime() {
+        return expirationTime;
     }
 }
